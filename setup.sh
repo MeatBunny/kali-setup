6d68ce3737113e0f15dca501500d3b073e698c15
root <root@freenas-linux.lab>
2019-04-04 19:33:02 -0400

    Removing built in SSH keys

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ usage () {
     echo -e "\t-p Don't install PTF."
     echo -e "\t-r Don't install PUPY RAT."
     echo -e "\t-l Don't setup root to log in automatically."
-    echo -e "\t-g Don't clone from github."
+    echo -e "\t-g Don't clone select repos from github to /opt."
     echo -e "\t-v Verbose"
     echo -e "\t-h This message"
     exit 1
@@ -25,7 +25,7 @@ usage () {
 
 debug () {
     if [[ $_verbose ]]; then
-        echo -e "\e[91mDebug:  $1\e[0m"
+        echo -e "\e[92mDebug:  $1\e[0m"
         sleep 1
     fi
 }
@@ -42,8 +42,8 @@ shopt -s nocasematch
 export DEBIAN_FRONTEND=noninteractive
 # Packages to install after update.
 _aptpackages="open-vm-tools-desktop vim htop veil-* docker.io terminator git libssl1.0-dev libffi-dev python-dev python-pip tcpdump python-virtualenv"
-_githubclone="chokepoint/azazel gaffe23/linux-inject nathanlopez/Stitch mncoppola/suterusu nurupo/rootkit trustedsec/ptf"
-_dockercontainers="alxchk/pupy:unstable empireproject/empire kalilinux/kali-linux-docker"
+_githubclone="chokepoint/azazel gaffe23/linux-inject nathanlopez/Stitch mncoppola/suterusu nurupo/rootkit"
+_dockercontainers="alxchk/pupy:unstable empireproject/empire kalilinux/kali-linux-docker python"
 
 unset _skipdocker _skipptf _skippupyrat _skipautologin _skipgithub _verbose
 while getopts 'hdprlgv' flag; do
@@ -59,24 +59,42 @@ while getopts 'hdprlgv' flag; do
     esac
 done
 
+if ! [[ $_skipautologin ]]; then
+    debug "Setting up root to automatically log in."
+    sed -i "s/^#.*AutomaticLoginEnable/AutomaticLoginEnable/g ; s/#.*AutomaticLogin/AutomaticLogin/g" /etc/gdm3/daemon.conf
+fi
+
 if ! [[ -f ~/.updated ]]; then
-    echo "Updating everything!"
+    debug "Removing built in SSH keys"
+    rm -vf /etc/ssh/ssh_host_*
+    dpkg-reconfigure openssh-server
+    debug "Updating everything.  Killing packagekitd in the background."
     pgrep packagekitd | xargs kill 2>/dev/null
     sleep 1
-    apt-get update && \
-    apt-get dist-upgrade -y && \
+    debug "Updating repos."
+    apt-get -q update
+    debug "Updating installed packages."
+    apt-get -q dist-upgrade -y
+    debug "Installing the below packages:"
+    debug "$_aptpackages"
+    apt-get -q install -y $_aptpackages
+    debug "Autoremoving things we don't need anymore."
+    apt-get -q autoremove -y
+    debug "Checking to see if we need to reboot after the update due to a new kernel."
     touch ~/.updated
-    apt-get install -y $_aptpackages
-    apt-get autoremove -y
-    _runningkernel=$(uname -r)
-    _ondiskkernel=$(dpkg --list | awk '/linux-image-[0-9]+/ {print $2}' | sort -V | tail -1)
-    if ! [[ $_ondisk =~ $_running ]]; then
-        echo "Looks like the running kernel ($_runningkernel) doesn't match the on disk kernel ($_ondiskkernel)."
-        read -p "Reboot? [yN]"
-        if [[ ${REPLY,,} =~ ^y ]]; then
-            reboot
-        fi
+fi
+
+_running=$(uname -r)
+_ondisk=$(dpkg --list | awk '/linux-image-[0-9]+/ {print $2}' | sort -V | tail -1)
+if ! [[ $_ondisk == *${_running}* ]]; then
+    warn "Looks like the running kernel ($_running) doesn't match the on disk kernel ($_ondisk) after the update."
+    echo "You're probably going to want to reboot and run this script again to avoid errors with PTF."
+    read -p "Reboot? [yN]"
+    if [[ ${REPLY,,} =~ ^y ]]; then
+        reboot
     fi
+else
+    debug "Looks good ... continuing."
 fi
 
 if ! [[ $_skipgithub ]]; then
@@ -96,11 +114,8 @@ if ! [[ $_skipgithub ]]; then
 fi
 
 if ! [[ $_skipptf ]]; then
-    debug "Set up PTF.  PTF requires --update-all to be run in its working directory."
-    if [[ $_skipgithub ]]; then
-        debug "Grabbing PTF from github since we didn't do it earlier."
-        git clone  https://github.com/trustedsec/ptf /opt/ptf
-    fi
+    debug "Grabbing PTF from github."
+    git clone  https://github.com/trustedsec/ptf /opt/ptf
     cat << EOF > /opt/ptf/config/ptf.config
 BASE_INSTALL_PATH="/opt"
 LOG_PATH="src/logs/ptf.log"
@@ -109,6 +124,7 @@ IGNORE_THESE_MODULES=""
 INCLUDE_ONLY_THESE_MODULES="modules/pivoting/3proxy,modules/webshells/b374k,modules/powershell/babadook,modules/powershell/bloodhound,modules/post-exploitation/empire,modules/powershell/empire,modules/post-exploitation/creddump7,modules/pivoting/meterssh,modules/windows-tools/netripper,modules/pivoting/pivoter,modules/pivoting/rpivot,modules/windows-tools/sidestep,modules/webshells/*"
 IGNORE_UPDATE_ALL_MODULES=""
 EOF
+    debug "Set up PTF.  PTF requires --update-all to be run in its working directory."
     pushd /opt/ptf
     echo -en "use modules/install_update_all\nyes\n" | python ptf
     popd
@@ -139,11 +155,6 @@ if ! [[ $_skippupyrat ]]; then
     popd
 fi
 
-if ! [[ $_skipautologin ]]; then
-    debug "Setting up root to automatically log in."
-    sed -i "s/^#.*AutomaticLoginEnable/AutomaticLoginEnable/g ; s/#.*AutomaticLogin/AutomaticLogin/g" /etc/gdm3/daemon.conf
-fi
-
 debug "Misc Kali commands"
 debug "MSFDB init"
 msfdb init
@@ -151,3 +162,5 @@ msfdb start
 
 debug "Update mlocate"
 updatedb
+
+rm -vf ~/.updated
