9c07966a0c1dd48f9a4ca7dae26d2a430ab6ea57
MeatBunny <24983387+MeatBunny@users.noreply.github.com>
2020-04-09 22:30:42 -0400

    Better ssh-agent logic before github pull.

--- setup.sh
+++ setup.sh
@@ -220,6 +220,11 @@ fi
 if [[ $githubdotfiles ]]; then
     pushd /home/$autologinuser
     debug "Pulling dotfiles from ${sshuser}.  Adding ssh-keys."
+    # Code 0 means keys are added, 1 means working but no keys, 2 is no agent.
+    agentstatus=$(ssh-add -l >/dev/null 2>&1 ; echo $?)
+    if [[ $agentstatus -eq "2" ]]; then
+        eval $(ssh-agent)
+    fi
     for file in $(grep -irl 'PRIVATE KEY' .ssh/); do
         ssh-add $file
     done
